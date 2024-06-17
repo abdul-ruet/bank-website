08bc8b241a0e165157e146553cfa6092237db509
abdul-ruet <abdul.ruet@gmail.com>
2024-06-18 01:26:42 +0600

    final js file

---
+++ file.js
@@ -0,0 +1,13 @@
+
+// const loginBtn = document.getElementById('verify');
+// loginBtn.addEventListener("click", function(){
+//   conss
+// })
+const loginBtn = document.getElementById("verify");
+loginBtn.addEventListener("click", function () {
+  const loginArea = document.getElementById("login-area");
+  loginArea.style.display = "none";
+  const tArea = document.getElementById("t-area");
+  tArea.style.display = "block";
+  
+});
