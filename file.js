72ef5953dfaae9e16683fed105ea43a8134404aa
abdul-ruet <abdul.ruet@gmail.com>
2024-07-04 00:01:18 +0600

    update balance

--- file.js
+++ file.js
@@ -1,8 +1,6 @@
 
-// const loginBtn = document.getElementById('verify');
-// loginBtn.addEventListener("click", function(){
-//   conss
-// })
+
+// login button event handler
 const loginBtn = document.getElementById("verify");
 loginBtn.addEventListener("click", function () {
   const loginArea = document.getElementById("login-area");
@@ -11,3 +9,35 @@ loginBtn.addEventListener("click", function () {
   tArea.style.display = "block";
   
 });
+
+const depositBtn=document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+
+    const depositAmount=document.getElementById("depositAmount").value;
+    const depositNumber=parseFloat(depositAmount);
+    
+    // const currentDeposit= document.getElementById("currentDeposit").innerText;
+    // const currentDepositNumber= parseFloat(currentDeposit);
+    // const totalDeposit= depositNumber+currentDepositNumber;
+    // document.getElementById("currentDeposit").innerText =totalDeposit;
+    updateSpanText("currentDeposit", depositNumber);
+
+    updateSpanText("currentBalance", depositNumber);
+
+
+   
+
+    document.getElementById("depositAmount").value= "";
+
+    function updateSpanText(id){
+      const current= document.getElementById(id).innerText;
+      const currentNumber= parseFloat(current);
+      const totalBalance = depositNumber+currentNumber;
+      document.getElementById(id).innerText=totalBalance;
+
+
+    }
+
+
+});
+
